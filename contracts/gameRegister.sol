a456621e23f031fbbb58109932c592e756c2963f
mm <macalonabc30@gmaile.com>
2023-06-24 10:49:47 -0400

    add:gameRegister frame

---
+++ contracts/gameRegister.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.9;
+
+contract GameRegister {
+
+    // gameアドレスとgameDescriptionやgameTitleをmap
+
+    // ratingからgameアドレスを取得する
+    function getGameAddress(address ratingAddress) public {
+    }
+
+    // gameアドレスからメタデータを取得する
+    function getMetadata(address gameAddress) public {
+    }
+    
+    // gameアドレスからrating一覧を取得する
+    function getEatingAddress(address gameAddress)public {
+    }
+
+    // gameを登録する
+    function registerGameDiscription(address gmaeAddress,string calldata description,string calldata title)public {
+    }
+}
