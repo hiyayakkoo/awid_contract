8cdf52308bfd60dfdd169206db2e9ee22ba94e64
mm <macalonabc30@gmaile.com>
2023-06-24 11:01:59 -0400

    fix:場所を取得する

--- contracts/gameRegister.sol
+++ contracts/gameRegister.sol
@@ -9,24 +9,18 @@ contract GameRegister {
     }
 
     mapping ( address => gameDescription ) gameDescriptions;
-    mapping ( address => address ) ratingGame;
-    mapping ( address => address[] ) gameRating;
-
-    // ratingからgameアドレスを取得する
-    function getGameAddress(address ratingAddress) public view returns (string memory){
-    }
-
-    // gameアドレスからメタデータを取得する
-    function getMetadata(address gameAddress) public view returns (gameDescription memory){
-        return gameDescriptions[gameAddress];
-    }
     
     // gameアドレスからrating一覧を取得する
-    function getEatingAddress(address gameAddress)public {
+    function getEatingAddress(address gameAddress)public view {
     }
 
     // gameを登録する
     function registerGameDiscription(address gameAddress,string calldata description,string calldata title) public {
         gameDescriptions[gameAddress] = gameDescription({title:title,description:description});
+    }
+
+    // gameアドレスからメタデータを取得する
+    function getMetadata(address gameAddress) public view returns (gameDescription memory){
+        return gameDescriptions[gameAddress];
     }
 }
