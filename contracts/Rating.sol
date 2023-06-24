47945bf7421564a7eb5b1810247972069f4ea2da
mm <macalonabc30@gmaile.com>
2023-06-24 00:01:07 -0400

    add:基礎的な定数の作成

--- contracts/Rating.sol
+++ contracts/Rating.sol
@@ -4,4 +4,13 @@ import "./IEAS.sol";
 import "./ISchemaRegistry.sol";
 
 contract Rating {
+    IEAS public eas = IEAS(0xC2679fBD37d54388Ce493F1DB75320D236e1815e);
+    bytes32 public SCHEMA_ID = 0x0;// 後でちゃんと作っていれる
+
+    mapping ( address => bytes32 ) private users;
+
+    struct Rating {
+        address EOAAddress;
+        uint256 ratingValue;
+    }
 }
